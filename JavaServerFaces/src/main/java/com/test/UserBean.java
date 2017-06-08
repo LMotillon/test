4d06211691a3c42b064dc50fcfddd2dce48a924a
LMotillon <lmotillon@humanbooster.com>
2017-06-08 11:54:09 +0200

    ajout gitignore modifié

---
+++ JavaServerFaces/src/main/java/com/test/UserBean.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+ 
+@ManagedBean(name="user")
+@SessionScoped
+public class UserBean implements Serializable{
+	private static final long serialVersionUID = -697512885431108712L;
+
+	//...
+	
+	
+}
